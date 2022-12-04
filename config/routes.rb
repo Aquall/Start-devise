69c82532cf55aae33e62a0b4dc69b7bf86cc5c1d
Nikita <nikitka_bodr@mail.ru>
2022-12-04 08:00:27 +0300

    tests-with-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :people
+  #resources :dogs
+  namespace :v1 do  
+    resources :people do
+      resources :dogs
+    end
+  end
+
+
 
   root to: redirect("/", status: 200)
 
